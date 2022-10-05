467847fb13cc927116967d561c542865f2c714cd
rivissolutions <72241997+sijumoncy@users.noreply.github.com>
2022-10-05 16:45:00 +0530

    tab changes locations

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import styled from 'styled-components'
-
+import { siteContent, devices } from "../Contents/SiteContents";
 
 function Packages() {
 
@@ -21,7 +21,30 @@ function Packages() {
           <div id="luxury" onClick={(e) => setActiveTab(e.target.id)}
             className={activeTab === 'luxury' ? 'active-tab' : ''}>Luxury</div>
         </div>
-        <div className='tab-content'></div>
+
+        <div className='tab-content'>
+          {siteContent.topPackages[activeTab].map((pack) => (
+            <div key={pack.id} className='content-container'>
+              <img src={pack.url} alt="No Prievew" />
+              <div className='destination'>
+                <h5>{pack.name}</h5>
+                <div className='locations'>
+                  {pack.locations.map((location, index) => (
+                    <span key={pack.id + index}>{location}</span>
+                  ))}
+                </div>
+                <div className="details">
+                  <div className="icons">ICONS</div>
+                  <div className='duration-price'>
+                    <div className="price">{pack.price}</div>
+                    <div className="duration">{pack.time}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
       </div>
     </Package>
   )
@@ -29,7 +52,6 @@ function Packages() {
 
 const Package = styled.section`
     width: 100%;
-    height: 80vh;
     background-color: aquamarine;
     display: flex;
     justify-content: center;
@@ -46,6 +68,8 @@ const Package = styled.section`
             gap: 2rem;
             font-weight: 400;
             font-size: small;
+            justify-content: center;
+            align-items: center;
             .active-tab{
                 display: flex;
                 flex-flow: column nowrap;
@@ -58,6 +82,51 @@ const Package = styled.section`
                     background-color: #801080;
                 }
             }
+        }
+        .tab-content{
+          display: flex;
+          flex-flow: column nowrap;
+          margin-top: 1rem;
+          .content-container{
+            margin: 0 1rem 0 1rem ;
+            img{
+              width: 100%;
+              height: 100%;
+              border-radius: .5rem;
+            }
+            .destination{
+              h5{
+                text-align: start;
+              }
+              .locations{
+                span{
+                  font-size: .5rem;
+                }
+              }
+              .details{
+                display: flex;
+                flex-flow: row nowrap;
+                justify-content: space-between;
+                align-items: center;
+                gap: .2rem;
+                .icons {
+                  font-size: .6rem;
+                }
+              }
+              .duration-price{
+                display: flex;
+                flex-flow: column nowrap;
+                gap: .2rem;
+                .price{
+                  font-size: .6rem;
+                }
+                .duration{
+                  font-size: .6rem;
+                }
+              }
+
+            }
+          }
         }
     }
 `;
